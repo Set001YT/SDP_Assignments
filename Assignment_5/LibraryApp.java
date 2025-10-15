99d35ef670b2bbf731509f552f21416e825fd344
Asset Iglikov <aaset0645@gmail.com>
2025-10-15 14:06:23 +0500

    updated LibraryApp
    I made it so that the decorator was in the decorator like a Matryoshka doll in a Matryoshka doll

--- Assignment_5/LibraryApp.java
+++ Assignment_5/LibraryApp.java
@@ -10,21 +10,17 @@ public class LibraryApp {
 
 // Order 2: A textbook with a protective cover and a bookmark
         Book book2 = new TextBook("Algorithms and Data Structures");
-        book2 = new ProtectiveCoverDecorator(book2);
-        book2 = new BookmarkDecorator(book2);
+        book2 = new ProtectiveCoverDecorator(new BookmarkDecorator(book2));
         library.borrowBook(book2, 30);
 
-// Order 3: A novel with an audio version
+// Order 3: A novel with an audio version & bookmark
         Book book3 = new NovelBook("The Master and Margarita");
-        book3 = new AudioVersionDecorator(book3);
+        book3 = new AudioVersionDecorator(new BookmarkDecorator(book3));
         library.borrowBook(book3, 21);
 
 // Order 4: Full-Service Textbook
         Book book4 = new TextBook("Calculus");
-        book4 = new ProtectiveCoverDecorator(book4);
-        book4 = new BookmarkDecorator(book4);
-        book4 = new HighlighterDecorator(book4);
-        book4 = new AudioVersionDecorator(book4);
+        book4 = new ProtectiveCoverDecorator(new BookmarkDecorator(new HighlighterDecorator(new AudioVersionDecorator(book4))));
         library.borrowBook(book4, 60);
     }
 }
